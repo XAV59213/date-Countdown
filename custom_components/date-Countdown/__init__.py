7f3f7c725bff1701b2557813446a2b76255945b5
XAV59213 <153436936+XAV59213@users.noreply.github.com>
2025-06-01 12:24:41 +0200

    Create __init__.py

---
+++ custom_components/date-Countdown/__init__.py
@@ -0,0 +1,26 @@
+"""Custom component for Date Countdown in Home Assistant."""
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+DOMAIN = "date_countdown"
+PLATFORMS = ["sensor"]
+
+async def async_setup(hass: HomeAssistant, config: dict) -> bool:
+    """Set up the Date Countdown component."""
+    hass.data.setdefault(DOMAIN, {})
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up Date Countdown from a config entry."""
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    entry.async_on_unload(entry.add_update_listener(async_update_entry))
+    return True
+
+async def async_update_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    """Handle config entry updates."""
+    await hass.config_entries.async_reload(entry.entry_id)
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
